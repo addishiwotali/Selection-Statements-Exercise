0cccd60d5a0facfc6b35293089b0258db705bcb4
Addishiwot Ali <sosenamillion@yahoo.com>
2025-06-24 22:35:01 -0700

    completed Selection statement excercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,33 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+     static void Main(string[] args)
+        { 
+         var random = new Random(); 
+         var favNumber = random.Next(1,15);
+         int response = 0;
+
+
+         while (response != favNumber)
+         {
+             Console.WriteLine("Please enter a number between 1 and 15.");
+             response = int.Parse(Console.ReadLine());
+             
+             if (response < favNumber)
+             {
+                 Console.WriteLine($"Too low!\nyour guess; {response} ");
+             }
+            
+             else if (response > favNumber)
+             {
+                 Console.WriteLine($"Too high!\nyour guess; {response} ");
+             }
+
+             else
+             {
+                 Console.WriteLine($"you guessed it!\nYour guess; {response} ");
+             } 
+         }
         }
     }
 }
